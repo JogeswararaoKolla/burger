4e193b9d4863f3c3d24d1b704f0cbb75b7b27e31
Jogeswararao Kolla <jogeskolla@gmail.com>
2019-10-03 01:27:27 -0700

    base code to heroku

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,18 @@ const mysql = require("mysql");
 const path = require("path");
 
 require("dotenv").config();
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  port: process.env.PORT,
-  database: process.env.DATABASE
-});
+
+if (process.env.JAWSDB_URL) {
+  const connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    port: process.env.PORT,
+    database: process.env.DATABASE
+  });
+}
 
 connection.connect(function(err) {
   if (err) {
